c2733bbeaaa69f2270adafac48a9dfe36e18a2b9
leehwasub <hwasub1115@naver.com>
2019-11-04 19:07:50 +0900

    4.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component{
 
@@ -20,11 +21,28 @@ class App extends React.Component{
   }
 
   render(){
-    const { isLoading } = this.state;
-    return <div>{this.state.isLoading ? "Loading..." : this.state.movies.map(movie => {
-      console.log(movie);
-      return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.poster}/>
-    })}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+        ) : (
+        <div className="movies">
+        {movies.map(movie => (
+          <Movie 
+            key={movie.id} 
+            id={movie.id} 
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.medium_cover_image}/>
+        ))}
+        </div>
+        )}
+      </section>
+    );
   }
 
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie({id, year, title, summary, poster}){
-    return <h1>{title}</h1>
+    return (
+    <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movies__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <p className="movie__summary">{summary}</p>
+        </div>
+    </div>
+    );
 }
 
 Movie.propTypes = {
